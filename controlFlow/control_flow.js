bde21c622ec5cbf9521f0320b7e18f5388772d3c
gaston arango <gaston.arango@gmail.com>
2024-06-28 14:59:22 -0400

    Add Switchtype

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -23,3 +23,22 @@ if (isLoggedIn) {
     userMessage = "Please log in to access the system.";
 }
 console.log("User Message:", userMessage);
+
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+console.log("User Category:", userCategory);
+
