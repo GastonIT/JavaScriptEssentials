dd538a6acd187a13b6bbac613cd765d9587d44f9
gaston arango <gaston.arango@gmail.com>
2024-06-28 14:55:19 -0400

    Add loggedin var

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -12,3 +12,13 @@ console.log("Access Level:", accessLevel);
 
 let isLoggedIn = true;
 let userMessage;
+
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
